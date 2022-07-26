77650bdad4ed4c637a6879a3ed4091d12f29ac03
vinniecent <vincent@verbrugh.nl>
2022-07-26 11:39:40 +0200

    chore: jetstream customization and tweaks

---
+++ resources/views/vendor/jetstream/components/authentication-card-logo.blade.php
@@ -0,0 +1,35 @@
+<svg width="132px" height="121px" viewBox="0 0 132 121" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+    <g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+        <g id="Logo-AW-zwart" transform="translate(0.000000, 1.000000)">
+            <g id="Group" transform="translate(37.000000, 104.000000)" fill="#3376BC" fill-rule="nonzero">
+                <circle id="Oval" cx="11.9" cy="8.7" r="4.5"></circle>
+                <circle id="Oval" cx="4.2" cy="3.8" r="3.3"></circle>
+                <circle id="Oval" cx="20.8" cy="9.5" r="3.4"></circle>
+                <circle id="Oval" cx="27.3" cy="9.2" r="2.4"></circle>
+            </g>
+            <circle id="Oval" fill="#3376BC" fill-rule="nonzero" cx="35.2" cy="100.1" r="2.2"></circle>
+            <circle id="Oval" fill="#3376BC" fill-rule="nonzero" cx="35.7" cy="113.3" r="2.6"></circle>
+            <circle id="Oval" fill="#3376BC" fill-rule="nonzero" cx="21.7" cy="113" r="1.8"></circle>
+            <circle id="Oval" fill="#3376BC" fill-rule="nonzero" cx="24.4" cy="93.8" r="2.1"></circle>
+            <circle id="Oval" fill="#3376BC" fill-rule="nonzero" cx="17.6" cy="102.6" r="1.9"></circle>
+            <circle id="Oval" fill="#3376BC" fill-rule="nonzero" cx="9.2" cy="108.4" r="1.6"></circle>
+            <circle id="Oval" fill="#3376BC" fill-rule="nonzero" cx="11.6" cy="93.5" r="1.4"></circle>
+            <circle id="Oval" fill="#3376BC" fill-rule="nonzero" cx="1.1" cy="97.6" r="1.1"></circle>
+            <circle id="Oval" fill="#3376BC" fill-rule="nonzero" cx="28" cy="107.9" r="2.2"></circle>
+            <path d="M53.5,119.2 C53.5,119.2 84.7,116.5 99.8,111.6 C108.9,108.6 123,104.1 126.9,104.4 C139.2,105.6 125.1,117.5 113.3,119.2" id="Path" fill="#3376BC" fill-rule="nonzero"></path>
+            <g id="Group" transform="translate(19.000000, 0.000000)">
+                <path d="M2.2,5.3 C0.9,7.5 6.2,34.4 10.7,50.9 C16,69.9 21.4,91.7 23,93.8 C24.6,95.9 27.4,97.7 34.5,99.1 C43.5,100.9 63,102.6 68.3,102.8 C71.3,102.9 77.5,103.5 77.3,102.3 C76.3,97.4 76.3,97 75,92.8 C73.9,89.3 66.7,68 59.7,55.8 C40.7,23 23,3.6 17.9,2.2 C14.1,1.1 3.6,3.1 2.2,5.3 Z" id="Path" fill="#3376BC" fill-rule="nonzero"></path>
+                <path d="M67.3,101.1 C70.4,101.2 76.8,102.3 77.8,101.4 C78.7,100.6 78.2,96 76.9,91.5 C75.8,87.8 69,67.1 61.7,54.5 C42.1,20.5 25.2,2.9 19.9,1.4 C16.1,0.2 4.6,3 3.3,5.3 C4.3,2.8 16.3,-0.9 20.2,0.3 C25.5,1.9 43.7,21.3 63.3,55.3 C70.6,67.9 78.9,93.1 78.9,93.1 C78.9,93.1 80.8,99.6 80.7,101.7 C80.7,103 71.9,101.9 68.8,101.8" id="Path" stroke="#3376BC" stroke-width="2" fill="#3376BC" fill-rule="nonzero" stroke-linecap="round" stroke-linejoin="round"></path>
+                <g transform="translate(0.000000, 15.000000)" id="Path" stroke="#FFFFFF" stroke-width="2">
+                    <path d="M0.4,2.7 L37.9,0.4"></path>
+                    <path d="M6.7,30.4 L49.8,21.1"></path>
+                    <path d="M16.3,59.4 L62.6,43"></path>
+                    <path d="M41.9,86.5 L74.4,70.4"></path>
+                </g>
+                <polygon id="Path" fill="#FFFFFF" fill-rule="nonzero" points="32.9 29 30.9 34.9 25 32.8 22.9 38.7 17.1 36.6 19.1 30.8 13.2 28.7 15.3 22.8 21.2 24.9 23.2 19 29.1 21.1 27 26.9"></polygon>
+                <polygon id="Path" fill="#FFFFFF" fill-rule="nonzero" points="45.3 53.5 43.1 59.8 36.8 57.6 34.6 63.8 28.4 61.6 30.6 55.3 24.3 53.1 26.5 46.9 32.8 49.1 35 42.8 41.3 45 39 51.3"></polygon>
+                <polygon id="Path" fill="#FFFFFF" fill-rule="nonzero" points="59 79.8 56.5 86.8 49.5 84.3 47 91.3 40 88.8 42.5 81.8 35.4 79.3 37.9 72.3 44.9 74.8 47.4 67.8 54.4 70.3 52 77.3"></polygon>
+            </g>
+        </g>
+    </g>
+</svg>
